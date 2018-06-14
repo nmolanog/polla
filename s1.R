9965a757cbf43645017291c7afcf5feafa2f5b50
nmolanog <nmolanog@gmail.com>
2018-06-14 11:12:06 -0500

    added a graph just for fun

--- s1.R
+++ s1.R
@@ -15,3 +15,10 @@ for(i in file_names){
   xlsx_list[[i]]<-read.xlsx(i, sheet =1, colNames = TRUE)
 }
 xlsx_list%>%reduce(full_join)->z0
+z0%>%select(-PAIS)%>%apply(1,sum,na.rm=T)->bets
+z0%>%select(-PAIS)%>%apply(2,sum,na.rm=T)
+bets<-data.frame(PAIS=z0$PAIS,bets=bets)
+bets2<-bets
+bets2$PAIS<-factor(bets2$PAIS,levels=bets[order(bets$bets,decreasing=T),"PAIS"])
+bets2%>%ggplot(aes(x=PAIS,y=bets))+geom_bar(stat="identity")+theme_bw()+
+  scale_y_continuous(breaks=seq(1,14,1))+ coord_flip()
