51337a101a8e478b8ec80d2d551b2f0027b94727
nmolanog <nmolanog@gmail.com>
2018-07-11 10:13:29 -0500

    scripts to generate the partial report of scores. RData files for s2 and s3 were generated

---
+++ report.R
@@ -0,0 +1,18 @@
+#######################
+###load data from RA BASE (2)
+######################
+rm(list=ls())
+options(max.print=999999)
+setwd("/media/nicolas/DATOS/Rprojects/polla")
+library(here)
+library(bueri)
+library(tidyverse)
+library(openxlsx)
+library(stringr)
+list.files()%>%str_subset(".RData")
+load("scores.RData")
+load("scores_s2.RData")
+load("scores_s3.RData")
+
+list(scores,scores_s2,scores_s3)%>%reduce(full_join)->z0
+z0$total<-apply(z0[,2:4],1,sum,na.rm=T)
--- ronda3/s3.R
+++ ronda3/s3.R
@@ -3,7 +3,7 @@
 ######################
 rm(list=ls())
 options(max.print=999999)
-setwd("/media/nicolas/DATOS/Rprojects/polla/ronda2")
+setwd("/media/nicolas/DATOS/Rprojects/polla/ronda3")
 library(here)
 library(bueri)
 library(tidyverse)
@@ -15,8 +15,8 @@ xlsx_list<-NULL
 for(i in file_names){
   xlsx_list[[i]]<-read.xlsx(i, sheet =1, colNames = TRUE)
 }
-ronda3<-read.xlsx("/media/nicolas/DATOS/Rprojects/polla/ronda3/Formato_tercera_ronda.xlsx", sheet =1, colNames = TRUE)
-clasificados<-ronda3[,1]
+ronda4<-read.xlsx("/media/nicolas/DATOS/Rprojects/polla/ronda4/Formato_ronda4.xlsx", sheet =1, colNames = TRUE)
+clasificados<-ronda4[,1]
 xlsx_list%>%reduce(full_join)->z0
 z0%>%select(-PAIS)%>%apply(1,sum,na.rm=T)->bets
 z0%>%select(-PAIS)%>%apply(2,sum,na.rm=T)
@@ -26,15 +26,15 @@ bets2$PAIS<-factor(bets2$PAIS,levels=bets[order(bets$bets,decreasing=T),"PAIS"])
 bets2%>%ggplot(aes(x=PAIS,y=bets))+geom_bar(stat="identity")+theme_bw()+
   scale_y_continuous(breaks=seq(1,14,1))+ coord_flip()
 
-z0%>%filter(PAIS %in% clasificados)%>%select(-PAIS)%>%apply( 2,sum,na.rm=T)->score_s2
-scores<-data.frame(id=names(score_s2),score_s2=score_s2)
-scores$id<-factor(scores$id,levels=scores[order(scores$score_s2,decreasing = T),"id"])
+z0%>%filter(PAIS %in% clasificados)%>%select(-PAIS)%>%apply( 2,sum,na.rm=T)->score_s3
+scores<-data.frame(id=names(score_s3),score_s3=score_s3)
+scores$id<-factor(scores$id,levels=scores[order(scores$score_s3,decreasing = T),"id"])
 row.names(scores)<-NULL
 
-scores%>%ggplot(aes(x=id,y=score_s2))+geom_bar(stat="identity")+theme_bw()+
+scores%>%ggplot(aes(x=id,y=score_s3))+geom_bar(stat="identity")+theme_bw()+
   scale_y_continuous(breaks=seq(1,14,1))+ coord_flip()
 setwd("..")
-scores_s2<-scores
-save(scores_s2,file="scores_s2.RData")
+scores->scores_s3
+save(scores_s3,file="scores_s3.RData")
 
 
